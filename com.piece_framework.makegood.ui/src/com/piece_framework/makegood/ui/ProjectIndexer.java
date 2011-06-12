2dbc876fe020f21b83ce36fecc54e54a4ed49137
MATSUFUJI Hideharu <matsufuji2008@gmail.com>
2011-06-13 00:04:52 +0900

    Changed code to adjust the test class that does not inherit the testing framework class directly. (Issue #233)

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/ProjectIndexer.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/ProjectIndexer.java
@@ -13,6 +13,9 @@
 package com.piece_framework.makegood.ui;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.dltk.core.IType;
 import org.eclipse.dltk.core.ITypeHierarchy;
 import org.eclipse.dltk.core.ModelException;
@@ -23,29 +26,51 @@ public class ProjectIndexer extends AbstractProjectIndexer {
     public ProjectIndexer() {}
 
     @Override
-    public void doIndexing(IndexDocument document) {
+    public void doIndexing(final IndexDocument document) {
+        for (Job job: Job.getJobManager().find(null)) {
+            if (job.getName().startsWith("DLTK indexing")) {
+                job.addJobChangeListener(new JobChangeAdapter() {
+                    @Override
+                    public void done(IJobChangeEvent event) {
+                        ProjectIndexer.this.doIndexing(document);
+                    }
+                });
+                return;
+            }
+        }
+
         if (document.isExternal()) return;
 
-        boolean includeTest = false;
+        boolean isTestClass = false;
         try {
             IType[] types = document.getSourceModule().getAllTypes();
             for (IType type: types) {
-                ITypeHierarchy hierarchy = type.newSupertypeHierarchy(new NullProgressMonitor());
-                for (IType superType: hierarchy.getAllSupertypes(type)) {
-                    if (superType.getTypeQualifiedName().equals("PHPUnit_Framework_TestCase")) {
-                        includeTest = true;
-                        break;
-                    }
+                if (isTestClass(type)) {
+                    isTestClass = true;
+                    break;
                 }
-                if (includeTest) break;
             }
         } catch (ModelException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-        if (includeTest) {
+        if (isTestClass) {
             System.out.println(document.getPath() + " includes the test class.");
         }
+    }
+
+    private boolean isTestClass(IType type) throws ModelException {
+        if (type.getSuperClasses() == null) return false;
+
+        for (String superClassName: type.getSuperClasses()) {
+            if (superClassName.equals("PHPUnit_Framework_TestCase")) return true;
+        }
+
+        ITypeHierarchy hierarchy = type.newSupertypeHierarchy(new NullProgressMonitor());
+        for (IType superType: hierarchy.getAllSupertypes(type)) {
+            if (isTestClass(superType)) return true;
+        }
+        return false;
     }
 }
